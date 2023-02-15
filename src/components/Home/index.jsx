52110bcffbb8e5f8b461155b3f699173c6e2ca0e
asror <asroralisherov6@gmail.com>
2023-02-15 15:14:23 +0500

    house card finished

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GenCorousel from "../corousel";
+import HouseCard from "../HouseCard";
 import { Container } from "./style";
 
 export const Home = () => {
   return (
     <Container>
       <GenCorousel />
+      <HouseCard />
     </Container>
   );
 };
